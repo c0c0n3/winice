98081f6ecdd9ba4cc01458b64f6c86b7c4cb0d23
c0c0n3 <andrea.falconi@gmail.com>
2026-04-24 20:09:54 +0200

    implement self-contained build.

--- nix/flake.nix
+++ nix/flake.nix
@@ -18,14 +18,32 @@
       pkgs.callPackage ./winice/framework-dependent-zip.nix {
         inherit rid;
       };
+    mkSelfCont = pkgs: rid:
+      pkgs.callPackage ./winice/self-contained.nix {
+        inherit rid;
+      };
   in {
     packages.${macos} = {
       default = macos-pkgs.callPackage ./winice/devenv.nix {};
       winice-bare-osx-arm64 = mkFwDepZip macos-pkgs "osx-arm64";
+      winice-osx-arm64 = mkSelfCont macos-pkgs "osx-arm64";
+
+      # cross-platform builds (not working at the mo)
+      # winice-bare-win-x64 = mkFwDepZip macos-pkgs "win-x64";
+      # winice-bare-win-arm64 = mkFwDepZip macos-pkgs "win-arm64";
+      # winice-win-x64 = mkSelfCont macos-pkgs "win-x64";
+      # winice-win-arm64 = mkSelfCont macos-pkgs "win-arm64";
     };
     packages.${win} = {
       default = win-pkgs.callPackage ./winice/devenv.nix {};
       winice-bare-win-x64 = mkFwDepZip win-pkgs "win-x64";
+      winice-win-x64 = mkSelfCont win-pkgs "win-x64";
+
+      # cross-platform builds (not working at the mo)
+      # winice-bare-win-arm64 = mkFwDepZip win-pkgs "win-arm64";
+      # winice-bare-osx-arm64 = mkFwDepZip win-pkgs "osx-arm64";
+      # winice-osx-arm64 = mkSelfCont win-pkgs "osx-arm64";
+      # winice-win-arm64 = mkSelfCont win-pkgs "win-arm64";
     };
   };
 }
---
+++ nix/winice/self-contained.nix
@@ -0,0 +1,72 @@
+#
+# Build a self-contained release.
+# This build targets any environment where .NET 10 can run. The whole
+# .NET 10 runtime and dependent libs are bundled in a single executable.
+# The Nix derivation's output contains
+#
+# - `nice.exe` (Windows) or `nice` (other platforms) binary. The executable
+#    file output by the `dotnet publish` command.
+# - `nice.sha256` hash. Text file with the SHA-256 hash of the binary.
+#
+# You can run winice by executing directly the binary file.
+#
+# See also:
+# - https://learn.microsoft.com/en-us/dotnet/core/deploying/?pivots=cli#self-contained-deployment
+#
+{ stdenv
+, dotnetCorePackages
+, rid
+}:
+
+with (import ./util.nix { inherit dotnetCorePackages; });
+
+stdenv.mkDerivation rec {
+
+  inherit pname version src;
+
+  buildInputs = [ dot-sdk ];
+
+  buildPhase = ''
+    dotnet publish nice/nice.csproj \
+      ${build-flags.version} \
+      --runtime ${rid} \
+      ${build-flags.release} \
+      -p:SelfContained=true \
+      -p:PublishSingleFile=true \
+      -p:PublishTrimmed=false
+  '';
+
+  installPhase = ''
+    mkdir $out winice
+
+    cp ${dotnetPubDir rid}/nice* winice/
+    cd winice
+    sha256sum nice* > nice.sha256
+    mv * $out/
+  '';
+
+}
+# NOTE
+# ----
+# 1. Why `nice*`? Depending on the platform, the binary could be named
+# `nice.exe` (Windows) or just `nice` (other platforms). So we use a glob
+# to cater for both names.
+#
+# 2. Trimming. Not worth the trouble. Winice is tiny at the moment and
+# the self-contained file is about 75MB. Trimming brings down the file
+# size to 15MB, but also introduces some weird behaviour. For example,
+#
+# $ nix build .#winice-osx-arm64
+# $ result/nice -n 10 eza -l result/
+# .r-xr-xr-x 76M root  1 Jan  1970 nice
+# .r--r--r--  71 root  1 Jan  1970 nice.sha256
+# nice: Cannot process request because the process (21194) has exited.
+#
+# The docs do say trimming can cause trouble. Let's leave it at that.
+# See:
+# - https://learn.microsoft.com/en-us/dotnet/core/deploying/trimming/trim-self-contained
+#
+# 3. Why not use `buildDotnetModule`? Yeah, we could do that in the
+# future---see `self-contained.alt.nix`. But right now, it seems to
+# me it might cause extra headaches for the kind of cross-compiling
+# we'd like to do.
